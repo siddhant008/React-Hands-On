b7a6b6bed9beeff36f938462063e2f11651b0c85
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2021-04-12 14:39:18 +0530

    handson 7 created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const element = "Office Space";
+  const sr = logo;
+  const jsxatt = <img src={sr} width="25%" height="25%" alt="Office Space" />;
+  const itemName = [
+    {
+      Name: "DBS",
+      Rent: 50000,
+      Address: "Chennai",
+    },
+    {
+      Name: "DBS2",
+      Rent: 70000,
+      Address: "Chennai",
+    },
+    {
+      Name: "DBS3",
+      Rent: 80000,
+      Address: "Chennai",
+    },
+  ];
+  let colors = [];
+  itemName.map((item) => {
+    if (item.Rent <= 60000) colors.push("textRed");
+    else colors.push("textGreen");
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{element}, at Affordable Range</h1>
+      {jsxatt}
+      {itemName.map((item, index) => {
+        return (
+          <div>
+            <h1>Name: {item.Name}</h1>
+            <h3 className={colors[index]}>Rent: {item.Rent}</h3>
+            <h3>Address: {item.Address}</h3>
+            <hr />
+          </div>
+        );
+      })}
     </div>
   );
 }
